8062af0dd45c3f1afa409f0762cfde9ccf5922ec
Tom Chesters <tom@tomaaato.xyz>
2022-01-04 15:29:22 +0000

    Prefix actions with 'R3G_' (2)

--- packages/react-redux-rest-generator/src/generateRestClient/generateRestRedux/generateRestActions.ts
+++ packages/react-redux-rest-generator/src/generateRestClient/generateRestRedux/generateRestActions.ts
@@ -2,14 +2,14 @@ import { RestReduxActionsGenerator, RestReduxActionSet } from '../../types'
 import mapObj from '../../utils/mapObj'
 
 const actionsTemplate = {
-  SET_FIELD: 'R3G_SET_FIELD',
-  QUEUE_REQUEST: 'R3G_QUEUE_REQUEST',
-  CANCEL_REQUEST: 'R3G_CANCEL_REQUEST',
-  FETCH: 'R3G_FETCH',
-  RESPONSE: 'R3G_RESPONSE',
-  INVALIDATE: 'R3G_INVALIDATE',
-  CLEAR_FIELDS: 'R3G_CLEAR_FIELDS',
-  CLEAR_RESPONSE: 'R3G_CLEAR_RESPONSE',
+  SET_FIELD: 'SET_FIELD',
+  QUEUE_REQUEST: 'QUEUE_REQUEST',
+  CANCEL_REQUEST: 'CANCEL_REQUEST',
+  FETCH: 'FETCH',
+  RESPONSE: 'RESPONSE',
+  INVALIDATE: 'INVALIDATE',
+  CLEAR_FIELDS: 'CLEAR_FIELDS',
+  CLEAR_RESPONSE: 'CLEAR_RESPONSE',
 }
 
 const generateRestActions: RestReduxActionsGenerator = (resourceConfig) => {
@@ -17,7 +17,7 @@ const generateRestActions: RestReduxActionsGenerator = (resourceConfig) => {
 
   const actions = mapObj(actionsTemplate, (key, value) => ({
     key,
-    value: `${resourceName.toUpperCase()}_${value}`,
+    value: `R3G_${resourceName.toUpperCase()}_${value}`,
   }))
 
   return actions as RestReduxActionSet
